5e9ee806f41626650375b044db1189f82f281d36
Gary Greyling <greyling.gary@gmail.com>
2021-02-21 11:33:42 +0200

    bump 1.0.3

--- test-wp-plugin.php
+++ test-wp-plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name:       GH WP Plugin
  * Plugin URI:        https://github.com/Silicon-Vaalies/test-wp-plugin
  * Description:       My first GH WP Plugin
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            Andy Fragen
  * License:           GNU General Public License v2
  * License URI:       https://github.com/Silicon-Vaalies/test-wp-plugin
