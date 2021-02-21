1e7d595a1a53930d0eeaa64e19c5c740152b0ea2
Gary Greyling <greyling.gary@gmail.com>
2021-02-21 11:18:37 +0200

    bump version

--- test-wp-plugin.php
+++ test-wp-plugin.php
@@ -1,17 +1,16 @@
 <?php
 
 /**
- * Plugin Name:       GitHub Updater
+ * Plugin Name:       GH WP Plugin
  * Plugin URI:        https://github.com/Silicon-Vaalies/test-wp-plugin
  * Description:       My first GH WP Plugin
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Andy Fragen
  * License:           GNU General Public License v2
  * License URI:       https://github.com/Silicon-Vaalies/test-wp-plugin
  * Domain Path:       /languages
  * Text Domain:       github-updater
- * GitHub Plugin URI: https://github.com/afragen/github-updater
- * GitHub Languages:  https://github.com/afragen/github-updater-translations
+ * GitHub Plugin URI: https://github.com/Silicon-Vaalies/test-wp-plugin
  */
 
 add_action( 'wp_footer', 'my_function' );
