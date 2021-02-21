f281e1636b3c170d0700ef341120a2d9facaaad4
Gary Greyling <greyling.gary@gmail.com>
2021-02-21 11:02:20 +0200

    init

---
+++ test-wp-plugin.php
@@ -0,0 +1,20 @@
+/**
+ * Plugin Name:       GitHub Updater
+ * Plugin URI:        https://github.com/Silicon-Vaalies/test-wp-plugin
+ * Description:       My first GH WP Plugin
+ * Version:           1.0.0
+ * Author:            Andy Fragen
+ * License:           GNU General Public License v2
+ * License URI:       https://github.com/Silicon-Vaalies/test-wp-plugin
+ * Domain Path:       /languages
+ * Text Domain:       github-updater
+ * GitHub Plugin URI: https://github.com/afragen/github-updater
+ * GitHub Languages:  https://github.com/afragen/github-updater-translations
+ */
+
+add_action( 'wp_footer', 'my_function' );
+
+function my_function() {
+  echo 'hello world';
+}
+
